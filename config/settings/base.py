ebc7735af11b1eb8daf84d51e206ccf1785eee32
starcat37 <starcat37@korea.ac.kr>
2023-04-28 19:58:43 +0900

    pybo 로거 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -207,5 +207,9 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
